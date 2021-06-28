ee5aed97576a2e687fbc1f72ce0cdfbf0639a31f
DESKTOP-2SBVSE1\PC-14 <coffee357722@gmail.com>
2021-06-28 21:43:12 +0900

    range(a, b) : 범위에 b-1까지 포함
    range(a, b+1)
    : 범위에 b까지 포함

--- HELLOPYTHON/day01/myhw02.py
+++ HELLOPYTHON/day01/myhw02.py
@@ -2,7 +2,7 @@ num1 = input("시작 숫자 입력 : ")
 num2 = input("끝 숫자 입력 : ")
 
 sum = 0
-for i in range(int(num1), int(num2)):
+for i in range(int(num1), int(num2)+1):
     sum += i
     
 print("sum", sum)
